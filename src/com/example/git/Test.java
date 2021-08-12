2e14739455ae50ea3ae25ec8fb0efa5edea2c3e7
i.shesholko <~rr?[gujCw9_'4Vh>
2021-08-12 11:08:12 +0300

    xcxcv

--- src/com/example/git/Test.java
+++ src/com/example/git/Test.java
@@ -1,9 +1,14 @@
 package com.example.git;
 
+import java.math.BigDecimal;
+
 public class Test {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello git");
+		BigDecimal.valueOf(2.35);
+		System.getProperties();
+		System.out.println(System.getProperty("agent-user"));
 		return;
 	}
 
