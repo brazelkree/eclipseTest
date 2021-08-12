74afbf04a1b43b4bd9943068ce05c66aa71a4eb1
i.shesholko <~rr?[gujCw9_'4Vh>
2021-08-12 11:09:26 +0300

    222

---
+++ src/com/example/git/Test2.java
@@ -0,0 +1,14 @@
+package com.example.git;
+
+public class Test2 {
+	
+	private String field1;
+	
+	private int field2;
+	
+	public Test2(String field1, int field2) {
+		this.field1 = field1;
+		this.field2 = field2;
+	}
+
+}
